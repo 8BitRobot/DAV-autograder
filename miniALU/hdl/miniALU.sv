a6f9e422d6a1df8fe1f136750f964a87deb88ae9
Premkumar Giridhar <premgiridhar11@gmail.com>
2024-08-03 11:17:21 -0700

    updated

--- miniALU/hdl/miniALU.sv
+++ miniALU/hdl/miniALU.sv
@@ -1,14 +1,16 @@
 `timescale 1ns/1ns
 
 module miniALU(
-    input  logic [3:0]  operand1,
-    input  logic [3:0]  operand2,
-    input  logic        select,
-    output logic [19:0] result
+	input  logic [3:0]  operand1,
+	input  logic [3:0]  operand2,
+	input  logic        select,
+	output logic [19:0] result
 );
-    if (select) begin
-        result = operand1 + operand2;
-    end else begin
-        result = operand1 << operand2;
-    end
+	always_comb begin
+		if (select) begin
+			result = operand1 + operand2;
+		end else begin
+			result = operand1 << operand2;
+		end
+	end
 endmodule
