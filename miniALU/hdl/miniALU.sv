2263e69ee67dc1b1f80f5a1cb278f2f88c4673c4
bry4b <116151219+bry4b@users.noreply.github.com>
2024-10-15 16:24:30 -0700

    modified:   miniALU/hdl/miniALU.sv 	deleted:    test_miniALU.py

--- miniALU/hdl/miniALU.sv
+++ miniALU/hdl/miniALU.sv
@@ -1,16 +1,25 @@
 `timescale 1ns/1ns
 
-module miniALU(
-	input  logic [3:0]  operand1,
-	input  logic [3:0]  operand2,
-	input  logic        select,
-	output logic [19:0] result
-);
-	always_comb begin
-		if (select) begin
-			result = operand1 + operand2;
-		end else begin
-			result = operand1 << operand2;
-		end
-	end
+module miniALU (
+    // TODO: define your input and output ports
+    input logic [3:0] op1,
+    input logic [3:0] op2, 
+    input logic operation,
+    input logic sign, 
+    output logic [19:0] result
+    );
+
+    // The following block will contain the logic of your combinational circuit
+    always_comb begin
+        // TODO: write the logic for your miniALU here
+        if (~operation & ~sign) begin
+            result = op1 + op2;
+        end else if (~operation & sign) begin
+            result = op1 - op2;
+        end else if (operation & ~sign) begin
+            result = op1 << op2;
+        end else begin
+            result = op1 >> op2;
+        end
+    end
 endmodule
