9cf83ff2719ad7818e94728f84f3d5606cc405b2
Premkumar Giridhar <premgiridhar11@gmail.com>
2024-10-15 14:48:23 -0700

    updated autograder to lab 1

--- miniALU/hdl/miniALU.sv
+++ miniALU/hdl/miniALU.sv
@@ -1,10 +1,12 @@
 `timescale 1ns/1ns
-
-module miniALU(
-    input  logic [3:0]  operand1,
-    input  logic [3:0]  operand2,
-    input  logic        select,
+module miniALU (
+    // TODO: define your input and output ports
+    input logic [3:0] op1,
+    input logic [3:0] op2, 
+    input logic operation,
+    input logic sign, 
     output logic [19:0] result
-);
-    assign result = 1;
+    );
+
+    assign result = 'd1;
 endmodule
--- miniALU/test/test_miniALU.py
+++ miniALU/test/test_miniALU.py
@@ -13,9 +13,38 @@ async def miniALU_tb(dut):
         operand1 = random.randint(0, 15) # generate randomized input
         operand2 = random.randint(0, 15)
 
-        dut.operand1.value = operand1
-        dut.operand2.value = operand2
-        dut.select.value = 0
+        dut.op1.value = operand1
+        dut.op2.value = operand2
+
+        # operation = 0, sign = 0 -- addition
+        dut.operation.value = 0
+        dut.sign.value = 0
+
+        await cocotb.triggers.Timer(10, units='ns') # delay
+
+        exact = operand1 + operand2
+        computed = dut.result.value.integer # read output (unsigned)
+        assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}"  # If any assertion fails, the test fails, and the string would be printed in console
+
+        await cocotb.triggers.Timer(10, units='ns') # delay
+
+        # operation = 0, sign = 1 -- subtraction
+        dut.operation.value = 0
+        dut.sign.value = 1
+
+        await cocotb.triggers.Timer(10, units='ns') # delay
+
+        exact = operand1 - operand2
+        if exact < 0:
+            exact += 2**20
+        computed = dut.result.value.integer # read output (signed)
+        assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}"
+
+        await cocotb.triggers.Timer(10, units='ns') # delay
+
+        # operation = 1, sign = 0 -- left shift
+        dut.operation.value = 1
+        dut.sign.value = 0
 
         await cocotb.triggers.Timer(10, units='ns') # delay
 
@@ -25,15 +54,17 @@ async def miniALU_tb(dut):
 
         await cocotb.triggers.Timer(10, units='ns') # delay
 
-        dut.select.value = 1
+        # operation = 1, sign = 1 -- right shift
+        dut.operation.value = 1
+        dut.sign.value = 1
 
         await cocotb.triggers.Timer(10, units='ns') # delay
 
-        exact = operand1 + operand2
-        computed = dut.result.value.integer # read output (signed)
-        assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}"
+        exact = operand1 >> operand2
+        computed = dut.result.value.integer # read output (unsigned)
+        assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}"  # If any assertion fails, the test fails, and the string would be printed in console
 
-        print(f"Driven value: {exact} \t received value: {computed}") 
+        await cocotb.triggers.Timer(10, units='ns') # delay
 
 
 def test_miniALU():
