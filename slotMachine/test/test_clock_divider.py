acb35a0818ce165d7928df1286f9cc475d633df1
Premkumar Giridhar <premgiridhar11@gmail.com>
2024-10-15 16:34:06 -0700

    added clock divider test

--- slotMachine/test/test_clock_divider.py
+++ slotMachine/test/test_clock_divider.py
@@ -3,14 +3,13 @@ import cocotb.triggers
 import cocotb.clock
 import random
 
+import cocotb.utils
 from cocotb_test.simulator import run
 import pytest
 import glob
 
 @cocotb.test()
 async def clockDivider_tb(dut):
-    ''' Assign random values to input, wait for a clock and verify output '''
-
     # 50 MHz clock period
     period_50MHz = 20
 
@@ -21,79 +20,30 @@ async def clockDivider_tb(dut):
     # 25 MHz clock period
     period_25MHz = 40
     dut.speed.value = 25000000
-    dut.rst.value = 0
+    dut.rst.value = 1
 
     prev_time = None
     
-    for i in range(5):
-        print("hello")
+    for i in range(10):
         await cocotb.triggers.RisingEdge(dut.clk_div)
-        print(cocotb.utils.get_sim_time())
-        # if prev_time != None:
-        #     if prev_time + period_25MHz == 
-        # compare the current simulation time to prev_time
-        # reset prev_time
-        # repeat
+        current_sim_time = cocotb.utils.get_sim_time()
+        if prev_time != None:
+            assert prev_time + period_25MHz == current_sim_time, f"Failed to generate clock with period {period_25MHz}ns"
+        prev_time = current_sim_time
+
+    # 25 MHz clock period
+    period_10MHz = 100
+    dut.speed.value = 10000000
+    dut.rst.value = 1
+
+    prev_time = None
     
-    
-    '''
-    for i in range(50): # 50 experiments
-        operand1 = random.randint(0, 15) # generate randomized input
-        operand2 = random.randint(0, 15)
-
-        dut.op1.value = operand1
-        dut.op2.value = operand2
-
-        # operation = 0, sign = 0 -- addition
-        dut.operation.value = 0
-        dut.sign.value = 0
-
-        await cocotb.triggers.Timer(10, units='ns') # delay
-
-        exact = operand1 + operand2
-        computed = dut.result.value.integer # read output (unsigned)
-        assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}"  # If any assertion fails, the test fails, and the string would be printed in console
-
-        await cocotb.triggers.Timer(10, units='ns') # delay
-
-        # operation = 0, sign = 1 -- subtraction
-        dut.operation.value = 0
-        dut.sign.value = 1
-
-        await cocotb.triggers.Timer(10, units='ns') # delay
-
-        exact = operand1 - operand2
-        if exact < 0:
-            exact += 2**20
-        computed = dut.result.value.integer # read output (signed)
-        assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}"
-
-        await cocotb.triggers.Timer(10, units='ns') # delay
-
-        # operation = 1, sign = 0 -- left shift
-        dut.operation.value = 1
-        dut.sign.value = 0
-
-        await cocotb.triggers.Timer(10, units='ns') # delay
-
-        exact = operand1 << operand2
-        computed = dut.result.value.integer # read output (unsigned)
-        assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}"  # If any assertion fails, the test fails, and the string would be printed in console
-
-        await cocotb.triggers.Timer(10, units='ns') # delay
-
-        # operation = 1, sign = 1 -- right shift
-        dut.operation.value = 1
-        dut.sign.value = 1
-
-        await cocotb.triggers.Timer(10, units='ns') # delay
-
-        exact = operand1 >> operand2
-        computed = dut.result.value.integer # read output (unsigned)
-        assert exact == computed, f"Failed on the {i}th cycle. Got {computed}, expected {exact}"  # If any assertion fails, the test fails, and the string would be printed in console
-
-        await cocotb.triggers.Timer(10, units='ns') # delay
-    '''
+    for i in range(10):
+        await cocotb.triggers.RisingEdge(dut.clk_div)
+        current_sim_time = cocotb.utils.get_sim_time()
+        if prev_time != None:
+            assert prev_time + period_10MHz == current_sim_time, f"Failed to generate clock with period {period_10MHz}ns"
+        prev_time = current_sim_time
 
 
 def test_miniALU():
