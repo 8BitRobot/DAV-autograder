c5c4f77e0bb92d1073b756cee391e9877bfcd0dd
Adrianfrancis2 <adrianfrancisrozario@gmail.com>
2024-10-15 23:35:23 -0700

    clock divider skeleton

--- slotMachine/hdl/clock_divider.sv
+++ slotMachine/hdl/clock_divider.sv
@@ -1,27 +1,25 @@
-`timescale 1ns/1ns
+`timescale 1ns/1ns		//	This line is necessary for Questa testbenches, and for the auto-grader.
 
+//	Your other inputs are: 26-bit input "speed", 1-bit input "rst", and 1-bit reg output "clk_div". 
+//	The distinction between reg and wire can be confusing; feel free to use "logic" instead.
 module clock_divider #( parameter MAX_SPEED = 50000000 ) (
-	input clk,					//	input clk signal
-	input [25:0] speed,
-	input rst,
-	output reg clk_div		//	output clk signal
+	input clk					//	input clk signal
 );
 
 reg [25:0] count = 0;															//	count num pos edges
 reg [25:0] NEW_DIVISOR = 0;
-reg [25:0] DIVISOR = 0;															//	Max count before clk signal repeats
+reg [25:0] DIVISOR = 0;														//	Max count before clk signal repeats
 
-reg [1:0] rst_sr = 2'b00;
+reg [1:0] rst_sr = 2'b00;													//	Shift register to capture button press
 
-always @(posedge clk) begin													//	Activates on pos clk edge
-	count <= (count >= DIVISOR - 1 || rst_sr == 2'b01) ? 0 : count + 1;	//	Update counter
-	clk_div <= (count < DIVISOR / 2) ? 1 : 0;								//	High/low clk edge
-	DIVISOR <= NEW_DIVISOR;
-	rst_sr <= {rst_sr[0], rst};
+always @(posedge clk) begin												//	Activates on pos clk edge
+	//	Your counter should update on a clock edge. When should it reset? When should it increment?
+	//	Your output signal will be 0 or 1, based on your counter.
+	//	You will also need to assign your divisor value and reset shift-register value sequentially.
 end
 
 always_comb begin
-	NEW_DIVISOR = MAX_SPEED / speed;
+	NEW_DIVISOR = MAX_SPEED / speed;		//	NEW_DIVISOR changes directly based on the input "speed." What does NEW_DIVISOR represent?
 end
 
 endmodule
