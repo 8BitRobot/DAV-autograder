69663b6c7264cafc135da517543dbcebd7ec026c
Adrianfrancis2 <adrianfrancisrozario@gmail.com>
2024-10-16 00:48:01 -0700

    merge fix

--- miniALU/hdl/miniALU.sv
+++ miniALU/hdl/miniALU.sv
@@ -2,11 +2,7 @@
 
 module miniALU (
     // TODO: define your input and output ports
-<<<<<<< HEAD
-    // IMPORTANT: for the autograder to run correctly, you must use the port names we provide:
-=======
     // IMPORTANT: for the autograder to run correctly, you must use the port names we provide
->>>>>>> c5c4f77e0bb92d1073b756cee391e9877bfcd0dd
     op1,
     op2, 
     operation,
