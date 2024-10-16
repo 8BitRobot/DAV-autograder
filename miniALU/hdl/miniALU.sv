8ab3168472cbca9d74cf4aad283b72ea7b808560
bry4b <116151219+bry4b@users.noreply.github.com>
2024-10-15 23:12:09 -0700

    revert to skeleton code

--- miniALU/hdl/miniALU.sv
+++ miniALU/hdl/miniALU.sv
@@ -1,25 +1,17 @@
-`timescale 1ns/1ns
+`timescale 1ns/1ns // uncomment this line when you push to the autograder!
 
 module miniALU (
     // TODO: define your input and output ports
-    input logic [3:0] op1,
-    input logic [3:0] op2, 
-    input logic operation,
-    input logic sign, 
-    output logic [19:0] result
+    // IMPORTANT: for the autograder to run correctly, you must use the port names we provide
+    op1,
+    op2, 
+    operation,
+    sign, 
+    result
     );
 
     // The following block will contain the logic of your combinational circuit
     always_comb begin
         // TODO: write the logic for your miniALU here
-        if (~operation & ~sign) begin
-            result = op1 + op2;     //  test
-        end else if (~operation & sign) begin
-            result = op1 - op2;
-        end else if (operation & ~sign) begin
-            result = op1 << op2;
-        end else begin
-            result = op1 >> op2;
-        end
     end
 endmodule
